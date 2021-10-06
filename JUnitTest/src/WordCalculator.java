c6f076e83f6b99d91972ecef0602a93f458839e6
Ezequiel Zucoli <ezequielzucoli@hotmail.com>
2021-10-06 16:34:03 -0300

    Final Test

--- JUnitTest/src/WordCalculator.java
+++ JUnitTest/src/WordCalculator.java
@@ -1,8 +1,9 @@
+import java.util.Locale;
+
 public class WordCalculator {
 
-    public boolean esPalindromo(String word){
-
-        word = word.toLowerCase()
+    public String reemplaceWord(String word){
+        return word.toLowerCase()
                 .replace("á", "a")
                 .replace("é", "e")
                 .replace("í", "i")
@@ -11,8 +12,36 @@ public class WordCalculator {
                 .replace(" ", "")
                 .replace(".", "")
                 .replace(",", "");
+    }
 
+    public boolean esPalindromo(String word){
+        word = reemplaceWord(word);
         String invertida = new StringBuilder(word).reverse().toString();
         return invertida.equals(word);
+    }
+
+    public int contarVocales(String word){
+        word = reemplaceWord(word);
+        int contador = 0;
+        for(int x=0;x<word.length();x++) {
+            if ((word.charAt(x)=='a') ||
+                    (word.charAt(x)=='e') ||
+                    (word.charAt(x)=='i') ||
+                    (word.charAt(x)=='o') ||
+                    (word.charAt(x)=='u')){
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public boolean comienzaCon(String word, String inicio){
+        word = reemplaceWord(word);
+        boolean b1 = false;
+        if(b1 == word.startsWith(inicio)){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- JUnitTest/src/WordCalculatorTest.java
+++ JUnitTest/src/WordCalculatorTest.java
@@ -7,9 +7,32 @@ public class WordCalculatorTest {
     private WordCalculator wc = new WordCalculator();
 
     @Test
+    public void reemplaceWordTest(){
+        String result = wc.reemplaceWord("Casa");
+        assertEquals("casa",result);
+    }
+
+    @Test
     public void esPalindromoTest(){
-        String word = "neuquen";
-        boolean result = wc.esPalindromo(word);
+        boolean result = wc.esPalindromo("neuquen");
         assertEquals(true, result);
+    }
+
+    @Test
+    public void contarVocalesTest(){
+        int resutl = wc.contarVocales("murcielago");
+        assertEquals(5, resutl);
+    }
+
+    @Test
+    public void comienzaConTestTrue(){
+        boolean result = wc.comienzaCon("pipo","p");
+        assertEquals(false,result);
+    }
+
+    @Test
+    public void comienzaConTestFalse(){
+        boolean result = wc.comienzaCon("pipo","o");
+        assertEquals(true,result);
     }
 }
